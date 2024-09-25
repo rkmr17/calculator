f07f4ca7daed5cf77326a55b544fc33d474f549d
rkmr17 <137607169+rkmr17@users.noreply.github.com>
2024-09-25 23:31:46 +0900

    演算子変数名変更、ユーザー入力部実装

--- calculator.cpp
+++ calculator.cpp
@@ -4,7 +4,21 @@ int main() {
   std::cout << "電卓アプリケーション" << std::endl;
 
   double num1, num2, result;
-  char operator;
+  char op;
+
+//ユーザー入力
+std::cout << "最初の数値を入力してください：";
+std::cin >> num1;
+
+std::cout << "演算子を入力してください：";
+std::cin >> op;
+
+std::cout << "二つ目の数値を入力してください：";
+std::cin >> num2;
+
+std::cout << num1;
+std::cout << op;
+std::cout << num2;
 
   return 0;
 }
