512fcc5d65beff24031c6eaaf5090e2d4e04fdde
rkmr17 <137607169+rkmr17@users.noreply.github.com>
2024-09-27 22:14:11 +0900

    インデント整理

--- calculator.cpp
+++ calculator.cpp
@@ -6,36 +6,36 @@ int main() {
   double num1, num2, result;
   char op, keepRunning;
 
-do {
-  //ユーザー入力
-std::cout << "最初の数値を入力してください：";
-std::cin >> num1;
+  do {
+    //ユーザー入力
+  std::cout << "最初の数値を入力してください：";
+  std::cin >> num1;
 
-std::cout << "演算子を入力してください：";
-std::cin >> op;
+  std::cout << "演算子を入力してください：";
+  std::cin >> op;
 
-std::cout << "二つ目の数値を入力してください：";
-std::cin >> num2;
+  std::cout << "二つ目の数値を入力してください：";
+  std::cin >> num2;
 
-if (op == '+')
-  std::cout << "結果：" << num1 + num2 << std::endl;
-else if (op == '-')
-  std::cout << "結果：" << num1 - num2 << std::endl;
-else if (op == '*')
-  std::cout << "結果：" << num1 * num2 << std::endl;
-else if (op == '/')
-  if (num2 != 0)
-    std::cout << "結果：" << num1 / num2 << std::endl;
+  if (op == '+')
+    std::cout << "結果：" << num1 + num2 << std::endl;
+  else if (op == '-')
+    std::cout << "結果：" << num1 - num2 << std::endl;
+  else if (op == '*')
+    std::cout << "結果：" << num1 * num2 << std::endl;
+  else if (op == '/')
+    if (num2 != 0)
+      std::cout << "結果：" << num1 / num2 << std::endl;
+    else  
+      std::cout << "エラー:0で割ることはできません" << std::endl;
   else
-    std::cout << "エラー:0で割ることはできません" << std::endl;
-else
-  std::cout << "無効な演算子です" << std::endl;
+    std::cout << "無効な演算子です" << std::endl;
 
-  //ループ選択
-  std::cout << "もう一度計算しますか（y/n）:";
-  std::cin >> keepRunning;
+    //ループ選択
+    std::cout << "もう一度計算しますか（y/n）:";
+    std::cin >> keepRunning;
 
-} while (keepRunning == 'y');
-
-  return 0;
+  } while (keepRunning == 'y');
+  
+   return 0;
 }
