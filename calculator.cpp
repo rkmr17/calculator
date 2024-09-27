a1a81ae6bd3445b00fd4cce35fb2b054cca2b872
rkmr17 <137607169+rkmr17@users.noreply.github.com>
2024-09-27 22:12:38 +0900

    ループ処理実装

--- calculator.cpp
+++ calculator.cpp
@@ -4,9 +4,10 @@ int main() {
   std::cout << "電卓アプリケーション" << std::endl;
 
   double num1, num2, result;
-  char op;
+  char op, keepRunning;
 
-//ユーザー入力
+do {
+  //ユーザー入力
 std::cout << "最初の数値を入力してください：";
 std::cin >> num1;
 
@@ -29,6 +30,12 @@ else if (op == '/')
     std::cout << "エラー:0で割ることはできません" << std::endl;
 else
   std::cout << "無効な演算子です" << std::endl;
+
+  //ループ選択
+  std::cout << "もう一度計算しますか（y/n）:";
+  std::cin >> keepRunning;
+
+} while (keepRunning == 'y');
 
   return 0;
 }
